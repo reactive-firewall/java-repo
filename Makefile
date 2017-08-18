4917193604ba3ccb64d2a1eb5215e2391a6d26c7
reactive-firewall <reactive-firewall@users.noreply.github.com>
2017-08-17 17:21:33 -0700

    initial commit - copy-paste-modify

---
+++ Makefile
@@ -0,0 +1,235 @@
+#!/usr/bin/env make -f
+
+# Java Repo Template
+# ..................................
+# Copyright (c) 2017, Kendrick Walls
+# ..................................
+# Licensed under MIT (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# ..........................................
+# http://www.github.com/reactive-firewall/java-repo/LICENSE
+# ..........................................
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+ifeq "$(ECHO)" ""
+	ECHO=/bin/echo
+endif
+
+ifeq "$(LINK)" ""
+	LINK = ln -s -f
+endif
+
+ifeq "$(MAKE)" ""
+	MAKE=make
+endif
+
+ifeq "$(WAIT)" ""
+	WAIT=wait
+endif
+
+ifeq "$(INSTALL)" ""
+	INSTALL=install
+	ifeq "$(INST_OWN)" ""
+		INST_OWN=-o root -g staff
+	endif
+	ifeq "$(INST_OPTS)" ""
+		INST_OPTS=-m 755
+	endif
+endif
+
+ifeq "$(LOG)" ""
+	LOG=no
+endif
+
+ifeq "$(LOG)" "no"
+	QUIET=@
+endif
+
+ifeq "$(DO_FAIL)" ""
+	DO_FAIL=$(ECHO) "ok"
+endif
+
+CHGRP = chgrp
+CHMOD = chmod
+CHOWN = chown
+CP = /bin/cp -v
+EXIT = exit
+GENMANIFEST = ./genManifest.sh
+JAVAC = javac
+JAVADOC = javadoc
+JAVADOCTAGS = -protected -version -link http://download.oracle.com/javase/7/docs/api/ -charset "UTF-8" -windowtitle "java-repo Documentation" -author -group "Java Template Repository" org:org.* -sourcepath ./src/
+JARZIP = jar
+MAKE_OPTIONS = --no-print-directory
+MAKEWHATIS = /usr/libexec/makewhatis.local 
+MKDIR = mkdir -p
+MV = mv -v
+PROJECT_NAME = "java-repo"
+PRINT = printf
+PATCH = patch -f -i
+QUIET = @
+
+RM = rm -f
+RMDIR = rmdir
+SHELL = /bin/bash
+SYNC = sync
+TARGET_NAME = "javarepo"
+TOUCH = touch -am
+UNTAR = tar --extract
+UNTAR_OPTION = --file
+UNZIP = unzip
+UNZIP_OPTION = -q -u -o
+
+#reset time to January 01 2017 at 12:46:13 AM
+
+RESET_TIME = $(TOUCH) -c -f -t 201701010046.13
+
+PHONY: must_be_root cleanup
+
+safe:
+	$(QUIET)$(ECHO) "No target." ;
+	$(QUIET)$(EXIT) 0 ;
+
+init:
+	$(QUIET)$(ECHO) "$@: Done."
+
+build: ./src ./lib ./bin ./dist ./dist/$(TARGET_NAME).jar JavaDoc clean
+	$(QUIET)$(ECHO) "Built "$(PROJECT_NAME)"." ;
+
+install: build
+	$(QUIET)if [[ `uname -s` == "Darwin" ]] ; then $(MAKE) $(MAKE_OPTIONS) DIRECTIVE=install darwin ; else $(MAKE) $(MAKE_OPTIONS) DIRECTIVE=install linux ; fi
+	$(QUIET)$(ECHO) "Installed "$(PROJECT_NAME)"." ;
+
+darwin:
+	$(QUIET)$(CP) ./dist/Tools.jar /usr/local/share/java/$(TARGET_NAME).jar ; 
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(CHMOD) 0755 /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(CHOWN) 0 /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(CHGRP) staff /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(ECHO) "Installed "$(PROJECT_NAME)".jar." ;
+    
+linux:
+	$(QUIET)$(CP) ./dist/$(TARGET_NAME).jar /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(CHMOD) 0750 /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(CHOWN) 0 /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(CHGRP) bin /usr/local/share/java/$(TARGET_NAME).jar ;
+	$(QUIET)$(ECHO) "Installed "$(PROJECT_NAME)".jar." ;
+
+cleanup:
+	$(QUIET)$(RM) ./bin/org/*.class 2>/dev/null || true
+	$(QUIET)$(RM) ./bin/org/**/*.class 2>/dev/null || true
+	$(QUIET)$(RMDIR) ./bin/org/* 2>/dev/null || true
+	$(QUIET)$(RM) ./bin/*.class 2>/dev/null || true
+	$(QUIET)$(RMDIR) ./bin/org 2>/dev/null || true
+	$(QUIET)$(RMDIR) ./bin 2>/dev/null || true
+	$(QUIET)$(RM) tests/*.class 2>/dev/null || true
+	$(QUIET)$(RM) tests/*~ 2>/dev/null || true
+	$(QUIET)$(RM) src/*.class 2>/dev/null || true
+	$(QUIET)$(RM) src/*~ 2>/dev/null || true
+	$(QUIET)$(RM) *.class 2>/dev/null || true
+	$(QUIET)$(RM) bin/*/*.class 2>/dev/null || true
+	$(QUIET)$(RM) bin/*/*~ 2>/dev/null || true
+	$(QUIET)$(RM) *.DS_Store 2>/dev/null || true
+	$(QUIET)$(RM) bin/*.DS_Store 2>/dev/null || true
+	$(QUIET)$(RM) bin/*/*.DS_Store 2>/dev/null || true
+	$(QUIET)$(RM) jar/* 2>/dev/null || true
+	$(QUIET)$(RMDIR) jar 2>/dev/null || true
+	$(QUIET)$(RM) ./*/*~ 2>/dev/null || true
+	$(QUIET)$(RM) ./*~ 2>/dev/null || true
+	$(QUIET)$(RM) ./.*~ 2>/dev/null || true
+
+clean: cleanup
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(ECHO) "Cleaned "$(PROJECT_NAME)"." ;
+	$(QUIET)$(ECHO) "$@: Done."
+
+./bin: ./src
+	$(QUIET)$(MKDIR) $@
+	$(QUIET)$(CHMOD) 750 $@
+	$(QUIET)$(CHOWN) $(LOGNAME) $@
+	$(QUIET)$(JAVAC) -encoding UTF8 -classpath ./src/:**/*.jar -d $@ ./src/org/**/*.java
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(SYNC) ;
+	$(QUIET)$(ECHO) "Compiled "$(TARGET_NAME)"." ;
+
+./dist:
+	$(QUIET)$(MKDIR) $@
+	$(QUIET)$(CHMOD) 750 $@
+	$(QUIET)$(CHOWN) $(LOGNAME) $@
+
+./dist/$(TARGET_NAME).jar: ./bin ./dist ./resources ./resources/Manifest ./README
+	$(QUIET)$(JARZIP) cmf ./resources/Manifest ./dist/$(TARGET_NAME).jar -C ./bin .
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(JARZIP) uf ./dist/$(TARGET_NAME).jar README ;
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(RM) ./lib/.DS_Store 2>/dev/null ;
+	$(QUIET)$(WAIT) ;
+	$(QUIET)$(ECHO) "Bundled PakTools." ;
+
+./src:
+	$(QUIET)$(MKDIR) $@
+	$(QUIET)$(CHMOD) 750 $@
+	$(QUIET)$(CHOWN) $(LOGNAME) $@
+	$(QUIET)$(ECHO) "No Source Code ... You're Screwed." ;
+
+./resources/Manifest: ./bin ./resources
+	$(QUIET)$(ECHO) "Class-Path: "$(TARGET_NAME)".jar" > ./resources/Manifest ;
+	$(QUIET)$(GENMANIFEST) ./bin/org >> ./resources/Manifest ;
+	$(QUIET)$(WAIT) ;
+
+./resources:
+	$(QUIET)$(MKDIR) $@
+	$(QUIET)$(CHMOD) 750 $@
+	$(QUIET)$(CHOWN) $(LOGNAME) $@
+	$(QUIET)$(ECHO) "No Resource Files ... Need to Generate." ;
+
+./docs/stylesheet.css: ./docs
+	$(QUIET)$(ECHO) "... Whatever \"$@\" actually is, it is suffering from massive existence failure, try looking on another plane of existence ..." ;
+
+JavaDoc: ./docs ./docs/robots.txt
+	$(QUIET)$(ECHO) -n "Generating Documentation... "
+	$(QUIET)if [[ ( -e ./docs/stylesheet.css ) ]] ; then $(JAVADOC) -d ./docs/ $(JAVADOCTAGS) -stylesheetfile ./docs/stylesheet.css org.* ; else $(JAVADOC) -d ./docs/ $(JAVADOCTAGS) org.* ; fi
+	$(QUIET)$(ECHO) "DONE" ;
+
+./docs:
+	$(QUIET)$(MKDIR) $@
+	$(QUIET)$(CHMOD) 750 $@
+	$(QUIET)$(CHOWN) $(LOGNAME) $@
+
+./docs/robots.txt: ./docs
+	$(QUIET)$(ECHO) "User-agent: *" > $@
+	$(QUIET)$(ECHO) "Disallow: package-list" >> $@
+	$(QUIET)$(ECHO) "" >> $@
+	$(QUIET)$(ECHO) "User-agent: Googlebot" >> $@
+	$(QUIET)$(ECHO) "Disallow: *.html" >> $@
+
+uninstall:
+	$(QUITE)$(WAIT)
+	$(QUIET)$(ECHO) "$@: Done."
+
+purge: clean uninstall
+	$(QUIET)$(ECHO) "$@: Done."
+
+test: cleanup
+	$(QUIET)DO_FAIL=exit 2 ;
+	$(QUIET)$(DO_FAIL);
+	$(QUIET)$(ECHO) "$@: Done."
+
+test-style: cleanup
+	$(QUIET)$(ECHO) "$@: Done."
+
+must_be_root:
+	$(QUIET)runner=`whoami` ; \
+	if test $$runner != "root" ; then echo "You are not root." ; exit 1 ; fi
+
+%:
+	$(QUIET)$(ECHO) "No Rule Found For $@" ; $(WAIT) ;
+	$(QUIET)$(ECHO) "... MISSING UNKNOWN FILE \"$@\" ..." ;
+	$(QUIET)$(ECHO) "... Whatever \"$@\" actually is, it is suffering from massive existence failure, try looking on another plane of existence ..." ;
+
---
+++ src/org/InternalTool.java
@@ -0,0 +1,233 @@
+package org;
+import java.util.concurrent.RejectedExecutionException;
+import java.lang.Runnable;
+import java.util.Scanner;
+import java.io.InputStream;
+import java.io.File;
+
+/**
+ * A class to interface with an internal tool. Primarily for use with UNIX and
+ * Linux environments. This class primarily provides the boilerplate code for 
+ * {@link java.lang.Runtime#exec(java.lang.String, java.lang.String[], java.io.File)}.
+ * EXAMPLE CODE IS FROM PAK - programming accessory kit. 
+ * @author Kendrick Walls
+ * @version 20120921
+ * @since 20120622
+ */
+public class InternalTool {
+    
+    /**
+    */
+    private static boolean ISDEBUGMODE = FALSE;
+    
+    /**
+     Runs a given command and returns the output. Provides more boilerplate code than
+     {@link java.lang.Runtime#exec(java.lang.String, java.lang.String[], java.io.File) exec(String, String[], File)}
+     including i/o handling. If no output is emitted by the command the string
+     <code>NULL</code> will be returned. The command will attempt to run in the 
+     user's current directory as per the system property <code>user.dir</code>,
+     unless there is an error while ascertaining the user's directory. If there 
+     is an error determining the user's current directory then the command will
+     attempt to run in either the value of the system property
+     <code>java.io.tmpdir</code>, or the root directory.
+     CODE IS FROM PAK - programming accessory kit.
+     @param someTool The full tool's command text.
+     @return string the tool's output text.
+     @see java.lang.System#getProperty(java.lang.String)
+     @throws java.util.concurrent.RejectedExecutionException on error.
+     @since 20130302
+     */
+    public static java.lang.String useInternalTool(java.lang.String someTool)
+    {
+    /* the path to the working directory */
+    java.lang.String pwd;
+    
+    try {
+        pwd = java.lang.System.getProperty("user.dir", new java.lang.String("/").intern());
+    }
+    catch (SecurityException secErr) {
+        if ( org.InternalTool.ISDEBUGMODE ) {
+            System.out.println("User is in a directory Java can't access! - System may be hardened.");
+    };
+        try {
+            pwd = java.lang.System.getProperty("java.io.tmpdir", new java.lang.String("/").intern());
+        }
+        catch (SecurityException secondErr) {
+            if ( org.InternalTool.ISDEBUGMODE ) {
+                System.out.println("Java can't access temp! - System may be sand-boxed.");
+            };
+            pwd = new java.lang.String("/").intern();
+        }//SecurityException
+    }//SecurityException
+    
+    /* at this point the pwd should be valid */
+    try {
+        pwd = new java.io.File(pwd).getCanonicalPath().intern();
+    }
+    catch (java.io.IOException noSuchFile){
+        //throw noSuchFile;
+        pwd = new java.lang.String("/").intern();
+    }
+    
+    /*
+     at this point the working directory of the internal command will be the
+     value of pwd.
+     */
+    
+    java.lang.String data = new java.lang.String("").intern();
+    try {
+        java.util.Scanner input;
+        
+        java.lang.String theCommand = new java.lang.String( someTool ).intern();
+        
+        if ( org.InternalTool.ISDEBUGMODE ) {
+            pak.util.Log.logDebugMessage("Running: " + someTool);
+        };
+        
+        try {
+            
+            
+            java.lang.Process theInternalProc =
+            java.lang.Runtime.getRuntime().exec(theCommand, null, new java.io.File(pwd));
+            
+            try {
+                input = new java.util.Scanner( new java.io.BufferedInputStream(theInternalProc.getInputStream()) );
+                
+                boolean isNotDone = true;
+                
+                /*
+                 
+                 Absolutely DO NOT call a java.lang.Thread.yield();  here
+                 See java.lang.Runtime.getRuntime().exec(java.lang.String)
+                 
+                 ...it WILL misbehave
+                 
+                 */
+                
+                while (isNotDone) {
+                    try {
+                        theInternalProc.waitFor();
+                        isNotDone = false;
+                }
+                catch (java.lang.InterruptedException swapErr)
+                {
+                    isNotDone = true;
+                }
+                
+                }
+                    
+                    if (theInternalProc.exitValue() == 0)
+                        {
+                        if ( org.InternalTool.ISDEBUGMODE ) System.out.println("Success " + theInternalProc.exitValue());
+                    }
+                    else {
+                        System.err.println("Unknown Error: " + theInternalProc.exitValue());
+                    }
+                    
+            }
+            catch (Exception procErr) {
+                // catch the baton if any
+                RuntimeException theBaton =
+                new java.lang.RuntimeException("Shell Script Error!",
+                                               procErr);
+                theBaton.fillInStackTrace();
+                
+                if ( org.InternalTool.ISDEBUGMODE ) {
+                    System.err.println("Shell Script Error!");
+                    System.err.println("Sending exit signal");
+                };
+                theInternalProc.destroy();
+                // now toss the baton
+                throw theBaton;
+            }
+            
+            
+            //input.useDelimiter("\n");
+            if (input != null){
+                try // read records from file using Scanner object
+                {
+                synchronized (input)
+                    {
+                    while ( input.hasNextLine() )
+                        {
+                        // parse record contents
+                        String subtemp = input.nextLine();
+                        data += subtemp;
+                        if (input.hasNextLine()) data += "\n";
+                        } // end while
+                    }
+                } // end try
+                catch ( java.util.NoSuchElementException elementException )
+                {
+                System.err.println( "Process output improperly formed." );
+                System.err.println("Sending exit code to Internal Shell Script");
+                //System.exit( 1 );
+                } // end catch
+                catch ( IllegalStateException stateException )
+                {
+                System.err.println( "Error reading text from Process." );
+                System.err.println("Sending exit code to Internal Shell Script");
+                //System.exit( 1 );
+                } // end catch
+                finally {
+                    input.close();
+                    theInternalProc.destroy();
+                }
+            }
+            if (data == null || data.trim().length() <= 0) data = "NULL";
+            
+        }
+        catch (java.io.IOException ioErr)
+        {
+        
+        // catch the baton if any
+        RuntimeException theBaton =
+        new java.lang.RuntimeException("Internal Shell Script i/o Error!",
+                                       ioErr);
+        theBaton.fillInStackTrace();
+        
+        /*
+         
+         JUSTIFICATION OF END-USER ERROR
+         1. recovery is not straight-forward.
+         2. of the possible recovery options, either:
+         a. this code would violate the security of others,
+         b. OR this code would be knowingly susceptible to insertion attack and
+         THUS BE NEGLIGENT
+         3. recovery is only necessary in un-configured installation
+         (i.e. installing user's fault)
+         4. I can not in good conscience code a deliberately negligent error
+         correction. Errors have already occurred, and thus what is know is that
+         assumptions are already wrong, so attempting to recover may just cause
+         more errors and becomes a dangerous and difficult debugging process. 
+         
+         */
+        System.err.println("Internal Shell Script i/o Error!");
+        // might use someTool.split(" ")[0] for name ... 
+        System.err.println("Command is NOT in user's shell PATH scope.");
+
+         System.err.println("DEBUG MODE: Will continue to allow debugging!");
+         // now toss the original error baton
+         throw (java.util.concurrent.RejectedExecutionException)(theBaton);
+         
+        
+        } // end catch
+    }
+    catch (Exception err)
+        {
+        RuntimeException theBaton =
+        new java.lang.RuntimeException("Internal Tool Error!", err);
+        theBaton.fillInStackTrace();
+        data = "ERROR!";
+        throw (java.util.concurrent.RejectedExecutionException)(theBaton);
+        }
+    finally
+        {
+        return data;
+        }
+    }
+    
+    /* Colossians 3:23: Work hard and cheerfully at what ever you do,
+     as though you were working for the Lord rather than for people.  */
+    
+}
